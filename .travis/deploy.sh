6eb73055ad84afd5e82e6766f61fff815720dcda
Alexande B <abobrikovich@gmail.com>
2019-09-21 12:36:54 +0300

    Fix token decoding logic

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -3,5 +3,5 @@
 # Set an option to exit immediately if any error appears
 set -o errexit
 
-cat snap_token | snapcraft login --with -
-snapcraft push --release=edge latexml_*_amd64.snap
+snapcraft login --with .snapcraft/snapcraft.cfg
+snapcraft push --release=edge docker.snap
