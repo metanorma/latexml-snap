0f08d3b8ff93ad120494f9a4ac6848967b61ea05
Alexande B <abobrikovich@gmail.com>
2019-10-05 11:32:29 +0300

    Add snapcraft pack back

--- .snapcraft/fix_checksum.sh
+++ .snapcraft/fix_checksum.sh
@@ -4,6 +4,6 @@ set -o errexit
 
 cd /root 
 apt-get install -y squashfs-tools python3
-unsquashfs output/latexml_*_amd64.snap
-# snapcraft pack squashfs-root
-mksquashfs ./squashfs-root output/latexml_fixed.snap -noappend -comp xz -all-root -no-xattrs -no-fragments
+unsquashfs output/latexml.snap
+snapcraft pack squashfs-root --output output/latexml_fixed.snap
+# mksquashfs ./squashfs-root output/latexml_fixed.snap -noappend -comp xz -all-root -no-xattrs -no-fragments
