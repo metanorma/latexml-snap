59780d957714dd83dce1c142d1f8a5c159f560fa
Alexande B <abobrikovich@gmail.com>
2019-12-19 00:57:52 +0300

    Migrate to GH Actions

--- .snapcraft/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -o errexit
-
-cd /root
-apt update
-snapcraft --version
-snapcraft snap --output output/latexml.snap
--- .snapcraft/deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -o errexit
-
-cd /root
-snapcraft login --with .snapcraft/snapcraft.cfg
-snapcraft push --release=edge output/latexml_fixed.snap
